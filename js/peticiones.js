4645d9af82dbb2caf1fc512abc251ddb23889644
michael <chalamaicol9@gmail.com>
2022-10-08 10:30:37 -0500

    subi mi repositorio a git

---
+++ js/peticiones.js
@@ -0,0 +1,122 @@
+function leerClientes(){
+	$.ajax({    
+	    url : 'https://g8cd31088906c91-nzgay9a6046uu2lx.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/client/client',
+	    type : 'GET',
+	    dataType : 'json',
+
+	    success : function(clientes) {
+	   		let cs=clientes.items;
+	   		$("#listaClientes").empty();
+	   		for(i=0;i<cs.length;i++){
+	   			$("#listaClientes").append(cs[i].id+" <b>"+cs[i].name+"</b> "+cs[i].email+" "+cs[i].age);
+	   			$("#listaClientes").append("<button onclick='borrarCliente("+cs[i].id+")'>Borrar</button><br>");
+	   		
+	   		}
+	    },
+	    error : function(xhr, status) {
+	        alert('ha sucedido un problema');
+	    }
+	});
+}
+
+
+function guardarCliente() {
+	let idCliente=$("#idCliente").val();
+	let nombre=$("#nombreCliente").val();
+	let mailCliente=$("#mailCliente").val();
+	let edad=$("#edadCliente").val();
+
+	let data={
+		id:idCliente,
+		name:nombre,
+		email:mailCliente,
+		age:edad
+	};
+
+	let dataToSend=JSON.stringify(data);
+	
+
+
+	$.ajax({    
+	    url : 'https://g8cd31088906c91-nzgay9a6046uu2lx.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/client/client',
+	    type : 'POST',
+	
+	    data:dataToSend,
+	    contentType:'application/json',
+	    success : function(pepito) {
+	   		$("#idCliente").val("");
+			$("#nombreCliente").val("");
+			$("#mailCliente").val("");
+			$("#edadCliente").val("");
+	    },
+	    error : function(xhr, status) {
+	    },
+	    complete: function(){
+	    	leerClientes();
+	    }
+	});
+
+
+}
+
+
+function editarCliente(){
+	let idCliente=$("#idCliente").val();
+	let nombre=$("#nombreCliente").val();
+	let mailCliente=$("#mailCliente").val();
+	let edad=$("#edadCliente").val();
+
+	let data={
+		id:idCliente,
+		name:nombre,
+		email:mailCliente,
+		age:edad
+	};
+	let dataToSend=JSON.stringify(data);
+	$.ajax({    
+	    url : 'https://g8cd31088906c91-nzgay9a6046uu2lx.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/client/client',
+	    type : 'PUT',
+	    data:dataToSend,
+	    contentType:'application/json',
+	    success : function(pepito) {
+	   		$("#idCliente").val("");
+			$("#nombreCliente").val("");
+			$("#mailCliente").val("");
+			$("#edadCliente").val("");
+	    },
+	    error : function(xhr, status) {
+	   //     alert('ha sucedido un problema');
+	    },
+	    complete: function(){
+	    	leerClientes();
+	    }
+	});
+
+}
+
+function borrarCliente(idCliente){
+	let data={
+		id:idCliente
+	};
+	let dataToSend=JSON.stringify(data);
+	$.ajax({    
+	    url : 'https://g8cd31088906c91-nzgay9a6046uu2lx.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/client/client',
+	    type : 'DELETE',
+	    data:dataToSend,
+	    contentType:'application/json',
+	    success : function(pepito) {
+	   		$("#idCliente").val("");
+			$("#nombreCliente").val("");
+			$("#mailCliente").val("");
+			$("#edadCliente").val("");
+	    },
+	    error : function(xhr, status) {
+	   //     alert('ha sucedido un problema');
+	    },
+	    complete: function(){
+	    	leerClientes();
+	    }
+	});
+
+}
+
